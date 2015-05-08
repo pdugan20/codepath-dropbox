ecab3df505f4c141158f1b537e0b27525f2123b9
Patrick Dugan <patrick@nextdoor.com>
2015-05-07 17:34:17 -0700

    skeleton tab controller
    base tab controller for empty sign-up views

---
+++ codepath-dropbox/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  codepath-dropbox
+//
+//  Created by Patrick on 5/7/15.
+//  Copyright (c) 2015 Daily Doog Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = CGSize(width: 320, height: 1000)
+        scrollView.contentSize = imageView.image!.size
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
